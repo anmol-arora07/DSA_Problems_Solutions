1a7c383948f6fce94134a73e428a7b2c630f6d9e
Anmol Arora <anmolarora885@gmail.com>
2022-07-06 13:27:29 +0530

    inital commit

---
+++ array_strings/anagram_check.py
@@ -0,0 +1,30 @@
+
+def anagram (s1, s2):
+
+    s1= s1.replace(' ','').lower()
+    s2=s2.replace(' ','').lower()
+
+    count={}
+
+    for letter in s1:
+        if letter in count:
+            count[letter]+=1
+        else:
+            count[letter]=1
+
+    for letter in s2:
+        if letter in count:
+            count[letter]-=1
+        
+        else:
+            count[letter]=1
+
+    for k in count:
+        if count[k]!=0:
+            return False
+        
+        else:
+            return True
+
+
+print(anagram('client eastwood','old west action'))
